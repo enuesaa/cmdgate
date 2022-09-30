ea8ba023ec57db1fbfe375b803ffa1a1b0597d76
enuesaa <nsrciog@gmail.com>
2022-10-01 00:32:59 +0900

    chore: interface を決定

--- example.ts
+++ example.ts
@@ -1,15 +1,26 @@
-import {gateli, command, option} from './src/index'
+import {gateli, command, option, help, positional1, positional2, positionalRecursive, optionValue} from './src/a'
 
 gateli({
-  aaa: command(
-    {
-      aa: command({}, () => { console.log('aaa.aa'); return {} }),
-      input: option({}),
-    },
-    () => { console.log('aaa'); return {} }
-  ),
-  bbb: command({}, () => { console.log('bbb'); return {}}),
-  ccc: command({}, () => { console.log('ccc'); return {}}),
-  help: option({ short: 'h', long: 'help', global: true }),
+  _name: 'aaa',
+  _description: 'aaa command',
+  aa: command({
+    bb: command({
+      dd: command({})
+    }),
+    cc: option({
+      aa: optionValue({ _name: 'aa'}),
+      _required: true,
+      _description: ''
+    }),
+  }),
+  bbb: command({
+    bb2: command({
+      _handler: () => { console.log('bbb'); return {}},
+    }),
+  }),
+  ccc: command({
+    _handler: () => { console.log('bbb'); return {}},
+  }),
+  '--help': option({ alias: '-h', global: true }),
 })
 .exec()

c25726c66aa45be42af5d074a235131cc3658754
enuesaa <nsrciog@gmail.com>
2022-10-01 19:11:13 +0900

    feat: gateli の interface を変更

--- example.ts
+++ example.ts
@@ -1,47 +1,54 @@
-import { gateli, command, option, help, positional1, positional2, positionalArgs, optionValue } from './src/index'
-
-gateli({
-  _name: 'a',
-  _description: 'b',
-  add: command({
-    _handler: () => { console.log('add'); return {} }
-  }),
-  _handler: () => { console.log('a'); return {} }
-})
-.exec()
+import { gateli, command, option, help, positional1, positional2, positionalArgs, optionValue } from './src/a'
 
 // gateli({
-//   _name: 'example-package-manager',
-//   _description: 'example-package-manager cli',
-
-//   init: command({
-//     name: positional1(),
-//   }),
-
+//   _name: 'a',
+//   _description: 'b',
 //   add: command({
-//     name: positional1(),
-//     '--dev': option(),
+//     _handler: () => { console.log('add'); return {} }
 //   }),
-
-//   remove: command({
-//     name: positional1(),
-//   }),
-
-//   upgrade: command(),
-//   run: command(),
-
-//   search: command({
-//     name: positional1(),
-//   }),
-
-//   plguins: command({
-//     add: command(),
-//     remove: command(),
-//     search: command({
-//       name: positional1(),
-//     }),
-//   }),
-
-//   '--help': help({ alias: '-h', global: true }),
+//   _handler: () => { console.log('a'); return {} }
 // })
 // .exec()
+
+gateli({
+  name: 'example-package-manager',
+  description: 'example-package-manager cli',
+  with: {
+    'init': command({
+      with: {
+        'name': positional1()
+      }
+    }),
+    'add': command({
+      with: {
+        'name': positional1(),
+        '--dev': option(),       
+      }
+    }),
+    'remove': command({
+      with: {
+        name: positional1(),
+      },
+    }),
+    'upgrade': command(),
+    'run': command(),
+    'search': command({
+      with: {
+        'name': positional1(),
+      },
+    }),
+    'plguins': command({
+      with: {
+        'add': command(),
+        'remove': command(),
+        'search': command({
+          with: {
+            'name': positional1(),
+          },
+        }),
+      },
+    }),
+    '--help': help({ alias: '-h', global: true }),
+  }
+})
+.exec()

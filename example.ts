7e10e77254f44a4c3fb53f5e3b976b924c483c04
enuesaa <nsrciog@gmail.com>
2022-10-08 16:51:56 +0900

    fix: example.ts を tsconfig の include に入れた

--- example.ts
+++ example.ts
@@ -1,60 +1,55 @@
-import { gateli, command, option, help, positional1, positional2, positionals } from './src/index'
+import { gateli, command, option, help, positional1, positional2, positionals } from '@/index'
 
 gateli({
-  name: 'a',
-  description: 'b',
-  handler: () => { console.log('a'); return true },
+  name: 'example-package-manager',
+  description: 'example-package-manager cli',
   gate: {
-    add: command({
+
+    'version': command({
       handler: (arg) => {
-        console.log(arg)
-        console.log('add')
+        console.log('version: 0.1.0')
         return true
       }
-    })
+    }),
+
+    'init': command({
+      // gate: {
+      //   'name': positional1()
+      // }
+    }),
+
+    'add': command({
+      // gate: {
+      //   'name': positional1(),
+      //   '--dev': option(),       
+      // }
+    }),
+
+    'remove': command({
+      // gate: {
+      //   name: positional1(),
+      // },
+    }),
+
+    // 'upgrade': command(),
+    // 'run': command(),
+    // 'search': command({
+    //   gate: {
+    //     'name': positional1(),
+    //   },
+    // }),
+    // 'plguins': command({
+    //   gate: {
+    //     'add': command(),
+    //     'remove': command(),
+    //     'search': command({
+    //       gate: {
+    //         'name': positional1(),
+    //       },
+    //     }),
+    //   },
+    // }),
+    // '--help': help({ alias: '-h' }),
   }
 })
 .exec()
-
-// gateli({
-//   name: 'example-package-manager',
-//   description: 'example-package-manager cli',
-//   gate: {
-//     'init': command({
-//       gate: {
-//         'name': positional1()
-//       }
-//     }),
-//     'add': command({
-//       gate: {
-//         'name': positional1(),
-//         '--dev': option(),       
-//       }
-//     }),
-//     'remove': command({
-//       gate: {
-//         name: positional1(),
-//       },
-//     }),
-//     'upgrade': command(),
-//     'run': command(),
-//     'search': command({
-//       gate: {
-//         'name': positional1(),
-//       },
-//     }),
-//     'plguins': command({
-//       gate: {
-//         'add': command(),
-//         'remove': command(),
-//         'search': command({
-//           gate: {
-//             'name': positional1(),
-//           },
-//         }),
-//       },
-//     }),
-//     '--help': help({ alias: '-h' }),
-//   }
-// })
-// .exec()

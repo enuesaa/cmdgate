352a27c6a9e5a99a799b9f8b71b6cf917b5fc1c7
enuesaa <nsrciog@gmail.com>
2022-10-16 00:58:23 +0900

    fix: interface

--- examples/package-manager.ts
+++ examples/package-manager.ts
@@ -1,41 +1,43 @@
-import { cli, command, option, help, positional1 } from '../src/index'
+import { gateli } from '../src/a'
+import { cli, command, option, help, positional1, scope } from '../src/b'
 
 cli({
   name: 'package-manager',
   description: 'package manager cli',
   gate: {
+
+    'plugin': scope({
+      description: 'plugin',
+      gate: {
+        'list': command({
+          handler: ({ prompt }) => {
+
+          },
+        }),
+        'view': command({
+          handler: ({ prompt }) => {
+
+          },
+        }),
+        'add': command({
+          param: {
+            'name': positional1({ required: true }),
+            '--dev': option()
+          },
+          handler: ({ prompt }) => {
+
+          },
+        }),
+        '--help': help({ alias: '-h' }),
+      }
+    }),
+
     'version': command({
       handler: ({ prompt }) => {
         prompt.println('version: 0.1.0')
       },
     }),
 
-    'init': command({
-      gate: {
-        'name': positional1()
-      },
-      handler: ({ args, prompt }) => {
-        prompt.println(args)
-      },
-    }),
-
-    'add': command({
-      gate: {
-        'name': positional1(),
-        '--dev': option(),
-      },
-      handler: ({ args, prompt }) => {
-        prompt.println(args)
-      },
-    }),
-
-    'remove': command({
-      gate: {
-        name: positional1(),
-      },
-    }),
-
-    '--help': help({ alias: '-h' }),
-  }
+    '--help': help(),
+  },
 })
-.exec()

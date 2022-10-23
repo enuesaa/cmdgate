9b8bc3ffab8cd6ad7f71262a15a3185c01ed25f0
enuesaa <nsrciog@gmail.com>
2022-10-23 16:46:24 +0900

    feat: add test for positional2

---
+++ tests/sub-command-positionals.spec.ts
@@ -0,0 +1,53 @@
+import { gateli, command, positional1, positional2 } from '../src/index'
+import { Prompt } from '../src/prompt'
+
+describe('sub command positionals', () => {
+  let mockPromptPrintln: jest.SpyInstance
+  beforeEach(() => {
+    mockPromptPrintln = jest.spyOn(Prompt.prototype, 'println').mockImplementation()
+  })
+  afterEach(() => {
+    mockPromptPrintln.mockRestore()
+  })
+
+  it('sub command positional1', () => {
+    gateli({
+      gate: [
+        command('', {}),
+        command('aaa', {
+          param: {
+            bbb: positional1({})
+          },
+          handler: ({ args,  prompt }) => {
+            prompt.println(`subcommand output. positional1 value is ${args.bbb}`)
+          },
+        })
+      ],
+    })
+    .pass(['aaa', 'val'])
+    .exec()
+
+    expect(mockPromptPrintln.mock.calls[0][0]).toBe('subcommand output. positional1 value is val')
+  })
+
+  it('sub command positional1 and positional2', () => {
+    gateli({
+      gate: [
+        command('', {}),
+        command('aaa', {
+          param: {
+            bbb: positional1({}),
+            ccc: positional2({}),
+          },
+          handler: ({ args,  prompt }) => {
+            prompt.println(`subcommand output. positional1 value is ${args.bbb}. positional2 value is ${args.ccc}`)
+          },
+        })
+      ],
+    })
+    .pass(['aaa', 'val1', 'val2'])
+    .exec()
+
+    expect(mockPromptPrintln.mock.calls[0][0]).toBe('subcommand output. positional1 value is val1. positional2 value is val2')
+  })
+})

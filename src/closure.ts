2ea82d4295af88e0af586d4c5ba10a11cb72790e
enuesaa <nsrciog@gmail.com>
2023-05-21 23:49:14 +0900

    feat: closure を使って値を保持する関数を現実的なラインに落とし込んだ

--- src/closure.ts
+++ src/closure.ts
@@ -7,17 +7,51 @@ export const createState = <T>(initialValue: T) => {
   }
 }
 
-type CommandState = {
-  name: string,
-}
-export const command = createState<CommandState>({
-  name: 'a',
-})
+// type CommandState = {
+//   name: string,
+// }
+// const command = createState<CommandState>({
+//   name: 'a',
+// })
+// const createCommand = {
+//   name: (name: string) => {
+//     const state = command.state()
+//     state.name = name
+//     command.setState(state)
+//   }
+// }
 
-const createCommand = {
-  name: (name: string) => {
-    const state = command.state()
-    state.name = name
-    command.setState(state)
-  }
+type Command = {
+  name: (name: string) => Command;
+  description: (description: string) => Command;
+  getState: () => any;
 }
+
+export const createCommand = () => {
+  const state = {
+    name: '',
+    description: '',
+  }
+
+  const methods: Command = {
+    getState: () => {
+      return state
+    },
+    name: (name) => {
+      state.name = name;
+      return methods
+    },
+    description: (description) => {
+      state.description = description;
+      return methods
+    }
+  }
+
+  return methods
+}
+
+const command = createCommand()
+command.name('aa').description('bb')
+
+console.log(command.getState())
+

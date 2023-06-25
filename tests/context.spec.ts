a426715e257ce6a590abb8874dda79b519fde5ff
enuesaa <nsrciog@gmail.com>
2023-06-25 19:16:56 +0900

    fix: d.tsを生成できていなかった

--- tests/context.spec.ts
+++ tests/context.spec.ts
@@ -1,6 +1,6 @@
-import { createHandler } from '@/index'
-import { Context } from '@/context'
-import { CommandManifest } from '@/handler/config'
+import { createHandler } from '../src/index'
+import { Context } from '../src/context'
+import { CommandManifest } from '../src/manifest'
 
 describe('context', () => {
   const normalArgv = ['node', 'main.js', 'thisiscontent', '--filename', 'aa.json', '-f', 'json']
@@ -24,48 +24,57 @@ describe('context', () => {
     .describeManifest()
 
   it('context.getArgv should return argv', () => {
-    const context = new Context(normalArgv, commandManifest, normalHandlerManifest)
+    const context = new Context(normalArgv, commandManifest)
+    context.pushHistory(normalHandlerManifest)
     expect(context.getArgv()).toStrictEqual(['node', 'main.js', 'thisiscontent', '--filename', 'aa.json', '-f', 'json'])
   })
 
   it('context.validate should return true when args are valid', () => {
-    const context = new Context(normalArgv, commandManifest, normalHandlerManifest)
+    const context = new Context(normalArgv, commandManifest)
+    context.pushHistory(normalHandlerManifest)
     expect(context.validate()).toStrictEqual(true)
   })
 
   it('context.validate should return false when argument does not passed', () => {
-    const context = new Context(['node', 'main.js'], commandManifest, normalHandlerManifest)
+    const context = new Context(['node', 'main.js'], commandManifest)
+    context.pushHistory(normalHandlerManifest)
     expect(context.validate()).toStrictEqual(false)
   })
 
   it('context.validate should return false when unknown option passed', () => {
-    const context = new Context(['node', 'main.js', 'thisiscontent', '--aaa', 'bbb'], commandManifest, normalHandlerManifest)
+    const context = new Context(['node', 'main.js', 'thisiscontent', '--aaa', 'bbb'], commandManifest)
+    context.pushHistory(normalHandlerManifest)
     expect(context.validate()).toStrictEqual(false)
   })
 
   it('context.validate should return false when required option does not passed', () => {
     // --filename is required.
-    const context = new Context(['node', 'main.js', 'thisiscontent'], commandManifest, normalHandlerManifest)
+    const context = new Context(['node', 'main.js', 'thisiscontent'], commandManifest)
+    context.pushHistory(normalHandlerManifest)
     expect(context.validate()).toStrictEqual(false)
   })
 
   it('context.getArgs should return argv without nodebin or filename', () => {
-    const context = new Context(normalArgv, commandManifest, normalHandlerManifest)
+    const context = new Context(normalArgv, commandManifest)
+    context.pushHistory(normalHandlerManifest)
     expect(context.getArgs()).toStrictEqual(['thisiscontent', '--filename', 'aa.json', '-f', 'json'])
   })
 
   it('context.getArgumentValue should return argument value', () => {
-    const context = new Context(normalArgv, commandManifest, normalHandlerManifest)
+    const context = new Context(normalArgv, commandManifest)
+    context.pushHistory(normalHandlerManifest)
     expect(context.getArgumentValue('content')).toStrictEqual('thisiscontent')
   })
 
   it('context.getOptionValue should return option value', () => {
-    const context = new Context(normalArgv, commandManifest, normalHandlerManifest)
+    const context = new Context(normalArgv, commandManifest)
+    context.pushHistory(normalHandlerManifest)
     expect(context.getOptionValue('--filename')).toStrictEqual('aa.json')
   })
 
   it('context.getOptionValue should return option value if alias passed', () => {
-    const context = new Context(normalArgv, commandManifest, normalHandlerManifest)
+    const context = new Context(normalArgv, commandManifest)
+    context.pushHistory(normalHandlerManifest)
     expect(context.getOptionValue('--format')).toStrictEqual('json')
   })
 })

f17e89c37e71c9446b8cd736937e95efe0e6499d
enuesaa <nsrciog@gmail.com>
2023-10-15 23:53:35 +0900

    test: enable to pass context tests.

--- tests/context.spec.ts
+++ tests/context.spec.ts
@@ -1,77 +1,44 @@
 import { createHandler } from '../src/index'
 import { Context } from '../src/context'
+import { type CliConfig } from '../src/cli'
 
 describe('context', () => {
-  // const normalArgv = ['node', 'main.js', 'thisiscontent', '--filename', 'aa.json', '-f', 'json']
-  // const rootHander = createHandler()
-  //   .argument('content', {})
-  //   .option('--filename', {
-  //     required: true,
-  //   })
-  //   .option('--format', {
-  //     alias: '-f'
-  //   })
-  //   .handle((context, prompt) => {})
-  // const commandConfig: CommandConfig = {
-  //   name: 'samplecmd',
-  //   description: 'sample command',
-  //   version: '',
-  //   middlewares: [],
-  //   handlers: {
-  //     '': rootHander,
-  //   },
-  // }
+  const normalArgv = ['node', 'main.js', 'thisiscontent', '--filename', 'aa.json', '-f', 'json']
+  const rootHander = createHandler()
+  rootHander.argument('content', { description: '' })
+  rootHander.flag('--filename', { required: true })
+  rootHander.flag('--format', { alias: '-f' })
+  rootHander.main((context, prompt) => {
 
-  // it('context.getArgv should return argv', () => {
-  //   const context = new Context(normalArgv, commandConfig)
-  //   expect(context.getArgv()).toStrictEqual(['node', 'main.js', 'thisiscontent', '--filename', 'aa.json', '-f', 'json'])
-  // })
+  })
 
-  // it('context.validate should return true when args are valid', () => {
-  //   const context = new Context(normalArgv, commandConfig)
-  //   expect(context.validate()).toStrictEqual(true)
-  // })
+  const commandConfig: CliConfig = {
+    name: 'samplecmd',
+    description: 'sample command',
+    version: '',
+    middlewares: [],
+    handlers: {
+      '': rootHander,
+    },
+  }
 
-  // it('context.validate should return false when argument does not passed', () => {
-  //   const context = new Context(['node', 'main.js'], commandConfig)
-  //   context.push(normalHandlerConfig)
-  //   expect(context.validate()).toStrictEqual(false)
-  // })
+  it('context.getArgv should return argv', () => {
+    const context = new Context(normalArgv)
+    expect(context.getArgv()).toStrictEqual(['node', 'main.js', 'thisiscontent', '--filename', 'aa.json', '-f', 'json'])
+  })
 
-  // it('context.validate should return false when unknown option passed', () => {
-  //   const context = new Context(['node', 'main.js', 'thisiscontent', '--aaa', 'bbb'], commandConfig)
-  //   context.push(normalHandlerConfig)
-  //   expect(context.validate()).toStrictEqual(false)
-  // })
+  it('context.getRawArgs should return argv without nodebin or filename', () => {
+    const context = new Context(normalArgv)
+    expect(context.getRawArgs()).toStrictEqual(['thisiscontent', '--filename', 'aa.json', '-f', 'json'])
+  })
 
-  // it('context.validate should return false when required option does not passed', () => {
-  //   // --filename is required.
-  //   const context = new Context(['node', 'main.js', 'thisiscontent'], commandConfig)
-  //   context.push(normalHandlerConfig)
-  //   expect(context.validate()).toStrictEqual(false)
-  // })
+  it('context.getRawPositionals should return positional arguments', () => {
+    const context = new Context(normalArgv)
+    expect(context.getRawPositionals()).toStrictEqual(['thisiscontent'])
+  })
 
-  // it('context.getArgs should return argv without nodebin or filename', () => {
-  //   const context = new Context(normalArgv, commandConfig)
-  //   context.push(normalHandlerConfig)
-  //   expect(context.getArgs()).toStrictEqual(['thisiscontent', '--filename', 'aa.json', '-f', 'json'])
-  // })
-
-  // it('context.getArgumentValue should return argument value', () => {
-  //   const context = new Context(normalArgv, commandConfig)
-  //   context.push(normalHandlerConfig)
-  //   expect(context.getArgumentValue('content')).toStrictEqual('thisiscontent')
-  // })
-
-  // it('context.getOptionValue should return option value', () => {
-  //   const context = new Context(normalArgv, commandConfig)
-  //   context.push(normalHandlerConfig)
-  //   expect(context.getOptionValue('--filename')).toStrictEqual('aa.json')
-  // })
-
-  // it('context.getOptionValue should return option value if alias passed', () => {
-  //   const context = new Context(normalArgv, commandConfig)
-  //   context.push(normalHandlerConfig)
-  //   expect(context.getOptionValue('--format')).toStrictEqual('json')
-  // })
+  it('context.getRawFlags should return flags.', () => {
+    const context = new Context(normalArgv)
+    expect(context.getRawFlags()['--filename']).toStrictEqual(['aa.json'])
+  })
 })

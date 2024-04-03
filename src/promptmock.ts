cc86b321d8a3e7f98d9885e478326f12a8dbe684
nua <enuesaa@gmail.com>
2024-04-04 01:10:33 +0900

    chore(prompt): remove deprecated methods.

--- src/promptmock.ts
+++ src/promptmock.ts
@@ -3,7 +3,6 @@ import util from 'node:util'
 
 export class PromptMock implements PromptInterface {
   public out: string = ''
-  public code: number = -1
 
   print(message: string) {
     const text = util.format(message)
@@ -16,23 +15,5 @@ export class PromptMock implements PromptInterface {
 
   error(message: string) {
     this.print(message + `\n`)
-  }
-
-  /**
-   * @deprecated
-   */
-  exit(code: number) {
-    // TODO: this method should be `never`. try os exit here not to override code.
-    if (this.code !== -1) {
-      return
-    }
-    this.code = code
-  }
-
-  /**
-   * @deprecated
-   */
-  isExited(): boolean {
-    return this.code !== -1
   }
 }

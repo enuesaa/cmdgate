16b3761c16a7a74f300c304a2187245b1d705cf0
nua <enuesaa@gmail.com>
2024-05-10 01:50:41 +0900

    fix(example): do not use argv class

--- examples/jsonedit/tests/index.test.ts
+++ examples/jsonedit/tests/index.test.ts
@@ -1,21 +1,21 @@
 import { describe, expect, it } from 'vitest'
 import { cli } from '../src/index'
-import { PromptMock, Argv } from '@enuesaa/cmdgate'
+import { PromptMock } from '@enuesaa/cmdgate'
 
 describe('jsonedit', () => {
   it('edit', () => {
-    cli.argv = new Argv(['node', './bin/jsonedit.js', 'tests/testdata.json', '--path', 'a', '--value', 'c'])
+    const argv = ['node', './bin/jsonedit.js', 'tests/testdata.json', '--path', 'a', '--value', 'c']
     const prompt = new PromptMock()
     cli.prompt = prompt
-    cli.run()
+    cli.run(argv)
     expect(prompt.out).toStrictEqual('{"a":"c"}\n')
   })
   
   it('--help', () => {
-    cli.argv = new Argv(['node', './bin/jsonedit.js', '--help'])
+    const argv = ['node', './bin/jsonedit.js', '--help']
     const prompt = new PromptMock()
     cli.prompt = prompt
-    cli.run()
+    cli.run(argv)
     expect(prompt.out).toStrictEqual(`jsonedit
 
 Flags:
